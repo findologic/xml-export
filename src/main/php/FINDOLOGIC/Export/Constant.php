e8786f22173bb4192e38cbea02c78a055fb42ab6
Tobias Graml <tobias.graml@nosto.com>
2023-03-20 14:17:19 +0100

    Add path constant to new schema

--- src/main/php/FINDOLOGIC/Export/Constant.php
+++ src/main/php/FINDOLOGIC/Export/Constant.php
@@ -5,4 +5,6 @@ namespace FINDOLOGIC\Export;
 class Constant
 {
     public static $XSD_SCHEMA_PATH = __DIR__ . '/../../../resources/findologic.xsd';
+    
+    public static $XSD_SCHEMA_PATH_20 = __DIR__ . '/../../../resources/findologic_20.xsd';
 }
